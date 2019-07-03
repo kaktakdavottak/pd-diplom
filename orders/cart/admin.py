7dc261125df7753c4af1c259891bdc1e582a2ed6
kaktakdavottak <semenmineev@gmail.com>
2019-07-03 23:44:15 +0300

    Проработка моделей Контактов

--- orders/cart/admin.py
+++ orders/cart/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Contact, Phone, Address
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_field = ['product']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'dt', 'status']
+    search_fields = ['user']
+    list_filter = ['user', 'dt', 'status']
+    inlines = [OrderItemInline]
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'user', None) is None:
+            obj.user = request.user
+        obj.save()
+
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+
+
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Address, AddressAdmin)
+
--- orders/cart/models.py
+++ orders/cart/models.py
@@ -6,10 +6,26 @@ from shop.models import Shop
 
 
 class Order(models.Model):
+    STATUS_NEW = 'New'
+    STATUS_CONFIRMED = 'Confirmed'
+    STATUS_COMPILED = 'Compiled'
+    STATUS_SHIPPED = 'Shipped'
+    STATUS_DELIVERED = 'Delivered'
+    STATUS_CANCELED = 'Canceled'
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'New'),
+        (STATUS_CONFIRMED, 'Confirmed'),
+        (STATUS_COMPILED, 'Compiled'),
+        (STATUS_SHIPPED, 'Shipped'),
+        (STATUS_DELIVERED, 'Delivered'),
+        (STATUS_CANCELED, 'Canceled')
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
                              blank=True, verbose_name='Пользователь')
     dt = models.DateField(auto_now_add=True, verbose_name='Дата')
-    status = models.BooleanField(verbose_name='Статус', default=False)
+    status = models.CharField(verbose_name='Статус', choices=STATUS_CHOICES,
+                              default=STATUS_NEW, max_length=50)
 
     def __str__(self):
         return 'Заказ: {}'.format(self.id)
@@ -25,14 +41,15 @@ class OrderItem(models.Model):
                               blank=True, verbose_name='Пользователь',
                               related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True,
-                                blank=True, verbose_name='Товар')
+                                blank=True, verbose_name='Товар',
+                                related_name='order_items')
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True,
                              blank=True, verbose_name='Магазин')
     quantity = models.PositiveIntegerField(verbose_name='Количество',
                                            default=1)
 
     def __str__(self):
-        return self.id
+        return '{}_{}'.format(self.order, self.product)
 
 
 # https://webdevblog.ru/modelirAovanie-polimorfizma-v-django/
@@ -40,20 +57,39 @@ class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
                              blank=True, verbose_name='Пользователь')
 
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
 
 class Phone(Contact):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Номер должен быть в формате: '+70000000'. Поддерживается до 15 цифр.")
-    phone = models.CharField(validators=[phone_regex], max_length=17,
+    phone_num = models.CharField(validators=[phone_regex], max_length=17,
                              blank=True, verbose_name='Номер телефона')
+
+    def __str__(self):
+        return '{}'.format(self.phone_num)
+
+    class Meta:
+        verbose_name = 'Телефон'
+        verbose_name_plural = 'Телефоны'
 
 
 class Address(Contact):
-    country = models.CharField(verbose_name='Страна', max_length=50)
     city = models.CharField(verbose_name='Город', max_length=50)
     street = models.CharField(verbose_name='Улица', max_length=50)
-    house = models.PositiveIntegerField(verbose_name='Дом')
+    building = models.PositiveIntegerField(verbose_name='Дом')
+    block = models.PositiveIntegerField(verbose_name='Корпус')
     apartment = models.PositiveIntegerField(verbose_name='Квартира')
+
+    def __str__(self):
+        return f'{self.city}, {self.street}, {self.building}, {self.block}, {self.apartment}'
+
+    class Meta:
+        verbose_name = 'Адрес'
+        verbose_name_plural = 'Адреса'
+
 
 
 
